f2c6190d6454567d9b4f3804d2d0361cb28073c4
olesyamaniuk <maniukolesya222@gmail.com>
2024-10-16 11:39:53 +0300

    update

--- js/slick.js
+++ js/slick.js
@@ -15,22 +15,22 @@ $(function () {
   });
 });
 
-// $(function () {
-//   $('.reviews__list').slick({
-//     infinite: true,
-//     slidesToShow: 1,
-//     slidesToScroll: 1,
-//     variableWidth: true,
-//     // autoplay: true,
-//     // autoplaySpeed: 3500,
-//     speed: 1000,
-//     dots: false,
-//     // centerMode: true,
-//     cssEase: 'linear',
-//     prevArrow: '.reviews-button-left',
-//     nextArrow: '.reviews-button-right',
-//   });
-// });
+$(function () {
+  $('.reviews__list').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    variableWidth: true,
+    autoplay: true,
+    autoplaySpeed: 3500,
+    speed: 1000,
+    dots: false,
+    // centerMode: true,
+    cssEase: 'linear',
+    prevArrow: '.reviews-button-left',
+    nextArrow: '.reviews-button-right',
+  });
+});
 
 $(function () {
   $('.advantages__list').slick({
